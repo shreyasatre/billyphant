3f69598aa081ca2aca4aca24dcc7583911a2616b
Shreyas <shreyas@atre>
2012-09-06 16:16:11 -0400

    Setting up Git for Billyphant.
    Signed-off-by: Shreyas <shreyas@atre>

---
+++ Billyphant/index.php
@@ -0,0 +1,19 @@
+<!-- @author Shreyas -->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Billyphant</title>
+</head>
+<body>
+
+	<!-- Billyphant members Sign In Form -->
+	<form>
+		<input type="text" name="username">
+		<input type="password" name="password">
+		<input type="submit" value="Sign In">
+	</form>
+
+</body>
+</html>
